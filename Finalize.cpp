aa5c41057f09aaf50f8ab93d38941329beb390ad
cng24 <60279272+cng24@users.noreply.github.com>
2020-05-18 17:25:23 -0700

    Create Finalize.cpp

---
+++ Finalize.cpp
@@ -0,0 +1,22 @@
+#include<iostream?
+#include "TTree.h"
+#include "TMath.h"
+
+int(){
+
+gROOT->cd();
+TTree * t = new TTree("t", "a tree");
+TRandom r;
+Float_t A, B, C;
+t->Branch("Rand", &A, "A/F");
+t->Branch("Ran", &B, "B/F");
+t->Branch("Ra", &C, "C/F");
+
+A = r.Rndm() * 10; 
+B = r.Rndm() * 10; 
+C = r.Rndm() * 10; 
+
+t->Fill();
+t->Scan()
+
+}
