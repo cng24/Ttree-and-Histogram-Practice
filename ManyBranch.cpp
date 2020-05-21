95c6fd62436d2b1aceb04a282e96a001966f5754
cng24 <60279272+cng24@users.noreply.github.com>
2020-05-20 17:15:44 -0700

    Update ManyBranch.cpp

--- ManyBranch.cpp
+++ ManyBranch.cpp
@@ -8,9 +8,9 @@ gROOT->cd();
 TTree * t = new TTree("t", "a tree");
 TRandom r;
 Float_t A, B, C;
-t->Branch("Rand", &A, "A/F");
-t->Branch("Ran", &B, "B/F");
-t->Branch("Ra", &C, "C/F");
+t->Branch("B1", &A, "A/F");
+t->Branch("B2", &B, "B/F");
+t->Branch("B3", &C, "C/F");
 
 A = r.Rndm() * 10; 
 B = r.Rndm() * 10; 
